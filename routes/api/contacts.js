5322c7ac177eacd0f9757ee4ffb37858af04ad4c
DimaSvirin <115728392+DimaSvirin@users.noreply.github.com>
2023-08-03 12:29:55 +0300

    last update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,81 +1,21 @@
-import express from "express"
-import Contact from "../../models/contact.js";
-import { HttpError } from '../../helpers/index.js';
+import express from "express";
+import contactController from '../../controllers/contact-controller.js'
 import schema from '../../schema.js'
 import {isValidId, isEmptyBody} from '../../middlewars/index.js'
 import validateBody from "../../decorators/validateBody.js";
 
 const contactsRouter = express.Router()
 
-contactsRouter.get('/', async (req, res, next) => {
-  try {
-  const result = await Contact.find();
-  res.json(result);    
-  } catch (error) {
-    res.status(500).json({message: "Server error",})
-  }
+contactsRouter.get('/', contactController.getAll);
 
-})
+contactsRouter.get('/:contactId', isValidId, contactController.getOneById);
 
-contactsRouter.get('/:contactId', isValidId, async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await Contact.findById(contactId);
-    if (!result) {
-      throw HttpError(404, `Contacts with id=${contactId} not found`)
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-})
+contactsRouter.post('/', isEmptyBody, validateBody(schema.contactAddSchema), contactController.addContact);
 
+contactsRouter.put('/:contactId', isValidId, validateBody(schema.contactAddSchema), contactController.updateById);
 
-contactsRouter.post('/', isEmptyBody, validateBody(schema.contactScheme), async (req, res, next) => {
-  try {
-    const { error } = schema.contactScheme.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-      const result = await Contact.create(req.body);
-      res.status(201).json(result);
-  } catch (error) {
-        next(error);
-  }
-})
+contactsRouter.patch('/:contactId/favorite', isValidId, isEmptyBody, validateBody(schema.contactUpdateFavoriteScheme), contactController.updateField);
 
-contactsRouter.put('/:contactId', isValidId, validateBody(schema.contactScheme), async (req, res) => {
-  const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true })
-  if (!result) {
-    throw HttpError(400, `Contact with id=${contactId} not found`);
-  }
-  res.json(result);
-});
-
-contactsRouter.patch('/:contactId/favorite', isValidId, isEmptyBody, validateBody(schema.contactUpdateFavoriteScheme), async (req, res) => {
-  const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
-  if (!result) {
-    throw HttpError(404, `Contact with id=${id} not found`);
-  }
-  res.json(result);
-});
-
-
-contactsRouter.delete('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await Contact.findByIdAndDelete(contactId);
-  if (!result) {
-    throw new Error(404, `Contacts with id=${contactId} not found`)
-    }
-    res.json({
-    message: "Contact deleted"
-  })  
-  } catch (error) {
-      next(error);
-  }
-})
+contactsRouter.delete('/:contactId', isValidId, contactController.deleteById);
 
 export default contactsRouter
